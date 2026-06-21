a0eb2712933a495ec621bfa5c58ad218e70d3387
LoganStocker <loganlouisstocker@gmail.com>
2026-06-21 15:49:52 -0400

    Correcion a conexion base de datos

--- SISGESCOLMADO/Capa_datos/Capa_de_acceso_a_Datos.cs
+++ SISGESCOLMADO/Capa_datos/Capa_de_acceso_a_Datos.cs
@@ -6,7 +6,7 @@ namespace SISGESCOLMADO.Capa_datos
     public class Conexion
     {
         private static string cadenaConexion =
-            @"Data Source=.\SQLEXPRESS;Initial Catalog=SISGESCOLMADO_BD;Integrated Security=True;TrustServerCertificate=True;";
+        @"Data Source=.;Initial Catalog=SISGESCOLMADO_BD;Integrated Security=True;TrustServerCertificate=True;";
 
         public static SqlConnection ObtenerConexion()
         {
--- SISGESCOLMADO/Program.cs
+++ SISGESCOLMADO/Program.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using SISGESCOLMADO.Capa_datos;
 using SISGESCOLMADO.Capa_presentacion;
+using System;
+using System.Windows.Forms;
 
 namespace SISGESCOLMADO
 {
+
     internal static class Program
     {
         [STAThread]
         static void Main()
-        {
+        {   
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
