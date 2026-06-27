56e2e7a26c4790fbf8caea8edcdb7188b00fa4d9
LoganStocker <loganlouisstocker@gmail.com>
2026-06-27 17:57:55 -0400

    Creacion de clase hija ventacontado

---
+++ SISGESCOLMADO/Capa_negocios/VentaContadocs.cs
@@ -0,0 +1,26 @@
+﻿namespace SISGESCOLMADO.Capa_negocios
+{
+    public class VentaContado : Transaccion
+    {
+        public VentaContado(int idProducto, int cantidad, decimal total)
+            : base(idProducto, cantidad, total)
+        {
+        }
+
+        public VentaContado() : base()
+        {
+        }
+
+        // Implementacion metodo abstract
+        public override string FinalizarTransaccion()
+        {
+            return $"Venta al contado finalizada. Total cobrado: ${total}";
+        }
+
+        // Sobreescritura metodo virtual
+        public override string ProcesarPago()
+        {
+            return $"Pago al contado recibido completo: ${total}.";
+        }
+    }
+}
