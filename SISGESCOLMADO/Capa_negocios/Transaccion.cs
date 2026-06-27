39d3c2af13e143d5e1f51c379a6de17b1c44800b
LoganStocker <loganlouisstocker@gmail.com>
2026-06-27 17:53:00 -0400

    Clase abstracta transaccion

---
+++ SISGESCOLMADO/Capa_negocios/Transaccion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SISGESCOLMADO.Capa_negocios
+{
+    public abstract class Transaccion
+    {
+        protected int idProducto;
+        protected int cantidad;
+        protected decimal total;
+        protected DateTime fecha;
+
+        // Constructor completo
+        public Transaccion(int idProducto, int cantidad, decimal total)
+        {
+            this.idProducto = idProducto;
+            this.cantidad = cantidad;
+            this.total = total;
+            this.fecha = DateTime.Now;
+        }
+
+        // Constructor vacio
+        public Transaccion()
+        {
+            this.idProducto = 0;
+            this.cantidad = 0;
+            this.total = 0;
+            this.fecha = DateTime.Now;
+        }
+
+        public int IdProducto => idProducto;
+        public int Cantidad => cantidad;
+        public decimal Total => total;
+        public DateTime Fecha => fecha;
+
+        // MÉTODO ABSTRACTO
+        public abstract string FinalizarTransaccion();
+
+        // MÉTODO VIRTUAL
+        public virtual string ProcesarPago()
+        {
+            return "Procesando pago genérico de $" + total;
+        }
+    }
+}
