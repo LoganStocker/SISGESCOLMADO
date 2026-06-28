dfc6b305e13e6dc3879ef5195ddd86d6d481d4da
LoganStocker <loganlouisstocker@gmail.com>
2026-06-28 11:18:31 -0400

    Creacion con delegate y lambda una gestor de alertas de credito vencido

---
+++ SISGESCOLMADO/Capa_negocios/GestorAlertas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using SISGESCOLMADO.Capa_datos;
+
+namespace SISGESCOLMADO.Capa_negocios
+{
+    // DELEGATE personalizado define la firma de un método que evalúa una fila de credito
+    public delegate bool FiltroCredito(DataRow fila);
+
+    public class GestorAlertas
+    {
+        // Metodo normal que usa el delegate mas lambda para filtrar creditos vencidos
+        public List<DataRow> ObtenerCreditosVencidos()
+        {
+            CreditoDT creditoDT = new CreditoDT();
+            DataTable tablaCreditos = creditoDT.ConsultarTodos();
+
+            // Se asigna una expresión LAMBDA al delegado FiltroCredito
+            FiltroCredito esVencido = (fila) => fila["Estado"].ToString() == "Vencido";
+
+            List<DataRow> creditosVencidos = new List<DataRow>();
+
+            foreach (DataRow fila in tablaCreditos.Rows)
+            {
+                if (esVencido(fila))
+                {
+                    creditosVencidos.Add(fila);
+                }
+            }
+
+            return creditosVencidos;
+        }
+    }
+}
